7746544a8e1119559bfd7a234d0d45d219fead2b
leliaoff <leliaoff@ya.ru>
2019-12-13 10:59:11 +0400

    htaccess
    Signed-off-by: leliaoff <leliaoff@ya.ru>

---
+++ public_html/system/logs/log-2019-12-13.php
@@ -0,0 +1,40 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 148
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Hooks has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Hooks.php 30
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Config has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Config.php 29
+DEBUG - 2019-12-13 09:57:53 --> Config Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 142
+DEBUG - 2019-12-13 09:57:53 --> Hooks Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 148
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_URI has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\URI.php 29
+DEBUG - 2019-12-13 09:57:53 --> URI Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 148
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Router has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Router.php 29
+DEBUG - 2019-12-13 09:57:53 --> No URI present. Default controller set.
+DEBUG - 2019-12-13 09:57:53 --> Router Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 142
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Output has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Output.php 29
+DEBUG - 2019-12-13 09:57:53 --> Output Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 148
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Input has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Input.php 29
+DEBUG - 2019-12-13 09:57:53 --> Input Class Initialized
+DEBUG - 2019-12-13 09:57:53 --> Global POST and COOKIE data sanitized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 148
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Language has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Language.php 27
+DEBUG - 2019-12-13 09:57:53 --> Language Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 142
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Base has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Base5.php 33
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; Controller has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Controller.php 30
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; Site_Controller has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\application\libraries\MY_Controller.php 22
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; Public_Controller has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\application\libraries\MY_Controller.php 45
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; Admin_Controller has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\application\libraries\MY_Controller.php 69
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; Home has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\application\controllers\home.php 3
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_Loader has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\libraries\Loader.php 29
+DEBUG - 2019-12-13 09:57:53 --> Loader Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\codeigniter\Common.php 142
+DEBUG - 2019-12-13 09:57:53 --> Helper loaded: url_helper
+DEBUG - 2019-12-13 09:57:53 --> Helper loaded: form_helper
+ERROR - 2019-12-13 09:57:53 --> Severity: 8192  --> Methods with the same name as their class will not be constructors in a future version of PHP; CI_DB_driver has a deprecated constructor C:\OSPanel\domains\astrobs.local\public_html\system\database\DB_driver.php 31
+DEBUG - 2019-12-13 09:57:53 --> Database Driver Class Initialized
+ERROR - 2019-12-13 09:57:53 --> Severity: Notice  --> Only variables should be passed by reference C:\OSPanel\domains\astrobs.local\public_html\system\database\DB.php 133
